4295cb475ba19f125f2244dab8c0879473738179
nikhilchopra08 <129540378+nikhilchopra08@users.noreply.github.com>
2023-09-09 20:01:46 +0530

    1st

---
+++ project-4/starter/app/app/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import styled from 'styled-components';
+
+function App() {
+
+  return (
+
+    // <div>hi</div>
+    <MainContainer>
+      <TopContainer>
+        <div className='logo'>
+          <img src='./images/logo.svg' alt='Logo' />
+        </div>
+
+        <div className='Search'>
+          <input placeholder='Search Food' />
+        </div>
+      </TopContainer>
+    </MainContainer>
+  );
+}
+
+export default App;
+
+
+const MainContainer = styled.div`
+background-color: #323334`;
+const TopContainer = styled.section``;
